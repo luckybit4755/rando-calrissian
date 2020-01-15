6a06fcf8d3358fb78be9ce5319b51a9bbbc1491d
Valerie Hammond <luckbit4755+git-em@gmail.com>
2020-01-15 14:06:11 -0500

    matrices ar working

--- html/demo/webgl/Trianglo.js
+++ html/demo/webgl/Trianglo.js
@@ -4,6 +4,9 @@ Trianglo.prototype = {
 	gl: function( canvas ) {
 		let gl = canvas.getContext( 'webgl' );
 		if ( !gl ) throw 'could not get webgl context';
+		gl.enable( gl.DEPTH_TEST );
+		gl.depthFunc( gl.LESS );
+		gl.clearColor( 0, 0, 0, 1 );
 		return gl;
 	},
 	program: function( gl, vertexSource, fragmentSource ) {
@@ -42,6 +45,9 @@ Trianglo.prototype = {
 		data = new Float32Array( data );
 		let location = gl.getUniformLocation( program, name );
 		gl.uniformMatrix4fv( location, false, data );
+	},
+	clear: function( gl ) {
+		gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 	},
 	triangles: function( gl, faces ) {
 		if ( faces ) {
@@ -85,10 +91,13 @@ Trianglo.prototype = {
 			let x = trianglo.rotateX( c, s );
 			let y = trianglo.rotateY( c, s );
 			let z = trianglo.rotateZ( c, s );
-			let m = trianglo.multiply( trianglo.multiply( x, y ), z );
+			let q = trianglo.scale( 0.66 );
+
+			let m = trianglo.multiply( trianglo.multiply( trianglo.multiply( x, y ), z ), q );
 
 			trianglo.matrix( gl, program, 'uMatrix', m );
 
+			trianglo.clear( gl );
 			trianglo.triangles( gl, [ 0, 1, 2 ] );
 
 			setTimeout( function() { requestAnimationFrame( draw ) }, 50 );
@@ -143,6 +152,8 @@ Trianglo.prototype = {
 		];
     },
     scale: function( x, y, z ) {
+		if ( 'undefined' === typeof( y ) ) { y = x }
+		if ( 'undefined' === typeof( z ) ) { z = x }
         return [
 			x, 0, 0, 0,
 			0, y, 0, 0,
@@ -151,15 +162,17 @@ Trianglo.prototype = {
         ]
     },
 	multiply: function( m1, m2 ) {
-		// first cut: use slow for loops
+		return this.fastMultiply( m1, m2 );
+	},
+	slowMultiply: function( m1, m2 ) {
 		let result = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
 		let idx = 0;
 		for ( let i = 0 ; i < 4 ; i++ ) {
-
 			for ( let j = 0 ; j < 4 ; j++, idx++ ) {
 				let row_m1 = i;
 				let col_m2 = j;
 
+				let symbolic = 'result[ ' + idx + ' ] = ';
 
 				let sum = 0;
 				for ( let k = 0 ; k < 4 ; k++ ) {
@@ -169,11 +182,36 @@ Trianglo.prototype = {
 					let idx_m2 = row_m2 * 4 + col_m2;
 
 					sum += m1[ idx_m1 ] * m2[ idx_m2 ];
+
+					symbolic += ' + m1[ ' + idx_m1 + ' ] * m2[ ' + idx_m2 + ' ]';
 				}
 
 				result[ idx ] = sum;
+
+				console.log( symbolic ); // I pulled a sneaky!
 			}
 		}
 		return result;
+	},
+	fastMultiply: function( m1, m2 ) {
+		// generated from slowMultiply 
+		return [
+			m1[  0 ] * m2[ 0 ] + m1[  1 ] * m2[ 4 ] + m1[  2 ] * m2[  8 ] + m1[  3 ] * m2[ 12 ],
+			m1[  0 ] * m2[ 1 ] + m1[  1 ] * m2[ 5 ] + m1[  2 ] * m2[  9 ] + m1[  3 ] * m2[ 13 ],
+			m1[  0 ] * m2[ 2 ] + m1[  1 ] * m2[ 6 ] + m1[  2 ] * m2[ 10 ] + m1[  3 ] * m2[ 14 ],
+			m1[  0 ] * m2[ 3 ] + m1[  1 ] * m2[ 7 ] + m1[  2 ] * m2[ 11 ] + m1[  3 ] * m2[ 15 ],
+			m1[  4 ] * m2[ 0 ] + m1[  5 ] * m2[ 4 ] + m1[  6 ] * m2[  8 ] + m1[  7 ] * m2[ 12 ],
+			m1[  4 ] * m2[ 1 ] + m1[  5 ] * m2[ 5 ] + m1[  6 ] * m2[  9 ] + m1[  7 ] * m2[ 13 ],
+			m1[  4 ] * m2[ 2 ] + m1[  5 ] * m2[ 6 ] + m1[  6 ] * m2[ 10 ] + m1[  7 ] * m2[ 14 ],
+			m1[  4 ] * m2[ 3 ] + m1[  5 ] * m2[ 7 ] + m1[  6 ] * m2[ 11 ] + m1[  7 ] * m2[ 15 ],
+			m1[  8 ] * m2[ 0 ] + m1[  9 ] * m2[ 4 ] + m1[ 10 ] * m2[  8 ] + m1[ 11 ] * m2[ 12 ],
+			m1[  8 ] * m2[ 1 ] + m1[  9 ] * m2[ 5 ] + m1[ 10 ] * m2[  9 ] + m1[ 11 ] * m2[ 13 ],
+			m1[  8 ] * m2[ 2 ] + m1[  9 ] * m2[ 6 ] + m1[ 10 ] * m2[ 10 ] + m1[ 11 ] * m2[ 14 ],
+			m1[  8 ] * m2[ 3 ] + m1[  9 ] * m2[ 7 ] + m1[ 10 ] * m2[ 11 ] + m1[ 11 ] * m2[ 15 ],
+			m1[ 12 ] * m2[ 0 ] + m1[ 13 ] * m2[ 4 ] + m1[ 14 ] * m2[  8 ] + m1[ 15 ] * m2[ 12 ],
+			m1[ 12 ] * m2[ 1 ] + m1[ 13 ] * m2[ 5 ] + m1[ 14 ] * m2[  9 ] + m1[ 15 ] * m2[ 13 ],
+			m1[ 12 ] * m2[ 2 ] + m1[ 13 ] * m2[ 6 ] + m1[ 14 ] * m2[ 10 ] + m1[ 15 ] * m2[ 14 ],
+			m1[ 12 ] * m2[ 3 ] + m1[ 13 ] * m2[ 7 ] + m1[ 14 ] * m2[ 11 ] + m1[ 15 ] * m2[ 15 ]
+		];
 	}
 };
